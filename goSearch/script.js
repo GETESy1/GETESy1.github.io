69b8a42446eeb36e3918415cd3029f506bc11a04
GETESy1 <127528314+GETESy1@users.noreply.github.com>
2024-08-12 02:04:05 +0800

    Update script.js

--- goSearch/script.js
+++ goSearch/script.js
@@ -3,7 +3,7 @@ document.getElementById('searchForm').addEventListener('submit', function(event)
     const searchInput = document.getElementById('searchInput').value;
     if (searchInput.trim() === '48447') {
         // 如果输入的是48447，则直接跳转到特殊URL
-        window.location.href = "D:\ARG计划\GETESy1.github.io\pan/2.JPG";
+        window.location.href = "getesy1.github.io\ARG计划\GETESy1.github.io\pan/2.JPG";
     } else if (searchInput.trim() !== '') {
         // 如果输入的是其他任何文本，则跳转到指定的URL
         window.location.href = "404";
