71d5c0b58416d9471c27e8c50e645913317323cc
GETESy1 <127528314+GETESy1@users.noreply.github.com>
2024-08-12 02:02:40 +0800

    1

--- goSearch/script.js
+++ goSearch/script.js
@@ -3,9 +3,9 @@ document.getElementById('searchForm').addEventListener('submit', function(event)
     const searchInput = document.getElementById('searchInput').value;
     if (searchInput.trim() === '48447') {
         // 如果输入的是48447，则直接跳转到特殊URL
-        window.location.href = "https://www.example.com/special";
+        window.location.href = "D:\ARG计划\GETESy1.github.io\pan/2.JPG";
     } else if (searchInput.trim() !== '') {
         // 如果输入的是其他任何文本，则跳转到指定的URL
-        window.location.href = "https://www.example.com/other";
+        window.location.href = "404";
     }
 });
