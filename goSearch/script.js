54b9fce45a84c5a60017deed2d21719f69ab0d8e
GETESy1 <127528314+GETESy1@users.noreply.github.com>
2024-08-23 22:23:44 +0800

    Update script.js

--- goSearch/script.js
+++ goSearch/script.js
@@ -3,7 +3,7 @@ document.getElementById('searchForm').addEventListener('submit', function(event)
     const searchInput = document.getElementById('searchInput').value;
     if (searchInput.trim() === '48447') {
         // 如果输入的是48447，则直接跳转到特殊URL
-        window.location.href = "https://vdse.bdstatic.com//192d9a98d782d9c74c96f09db9378d93.mp4?authorization=bce-auth-v1/40f207e648424f47b2e3dfbb1014b1a5/2021-07-12T02:14:24Z/-1/host/530146520a1c89fb727fbbdb8a0e0c98ec69955459aed4b1c8e00839187536c9";
+        window.location.href = "https://www.bilibili.com/video/BV1PK4y1S7Hd/?share_source=copy_web&vd_source=a779509a896f159489ea3da274c51b20";
     } else if (searchInput.trim() !== '') {
         // 如果输入的是其他任何文本，则跳转到指定的URL
         window.location.href = "404";
